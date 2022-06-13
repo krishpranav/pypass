e037871126009eade1b51234694f5826d416d0ce
krishpranav <krisna.pranav@gmail.com>
2022-06-13 11:56:54 +0530

    password generator + docs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pypass
 random password generator in python
+
+## Installation:
+```
+$ git clone https://github.com/pypass
+```
+
+## Running:
+```
+$ python3 main.py
+```
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import string
 import random
 
@@ -8,18 +6,54 @@ digits = list(string.digits)
 special_characters = list("!@#$%^&*()")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
-def generate_random_passwd():
-    length = int(input("Enter Passwd Length > "))
-    
-    alphabets_count = int(input("Enter alphabets count in password: "))
+def banner():
+    print("""
+            PYTHON PASSWORD GENERATOR V.1 
+                [creates random strong password]
+            
+            Github: https://github.com/krishpranav
+            Twitter: https://twitter.com/krishpranav5
+            Linkedin: https://linkedin.com/in/krishpranav
+          
+          """)
+
+def generate_random_password():
+	length = int(input("Enter password length >> "))
+
+	alphabets_count = int(input("Enter alphabets count in password: "))
 	digits_count = int(input("Enter digits count in password: "))
 	special_characters_count = int(input("Enter special characters count in password: "))
- 
- 	characters_count = alphabets_count + digits_count + special_characters_count
+
+	characters_count = alphabets_count + digits_count + special_characters_count
 
 	if characters_count > length:
 		print("Characters total count is greater than the password length")
 		return
 
-    
-    
+	password = []
+	
+	for i in range(alphabets_count):
+		password.append(random.choice(alphabets))
+
+
+	for i in range(digits_count):
+		password.append(random.choice(digits))
+
+
+	for i in range(special_characters_count):
+		password.append(random.choice(special_characters))
+
+	if characters_count < length:
+		random.shuffle(characters)
+		for i in range(length - characters_count):
+			password.append(random.choice(characters))
+
+
+	random.shuffle(password)
+
+	print("".join(password))
+
+
+
+
+generate_random_password()
